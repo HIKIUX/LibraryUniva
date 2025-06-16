62f729aaae63b64820a2c4c12f6167e8cf82c180
HIKIUX <kevin7arechiga@gmail.com>
2025-06-16 12:37:52 -0600

    Primer Pull
    Practica del uso de github

---
+++ Services/ILibraryService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using LibraryManager.Models;
+
+namespace LibraryManager.Services
+{
+
+    public interface ILibraryService
+    {
+        List<Book> GetAll();
+
+        Book? GetById(Guid id);
+        
+        void Add(Book book);
+    }
+}
